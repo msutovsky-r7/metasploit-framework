00d8007f6ef01a282ee776e517e84ba5673544a5
Martin Sutovsky <martin_sutovsky@rapid7.com>
2026-01-30 13:30:55 +0100

    Updates the script, adds dummy config path

--- tools/modules/update_payload_cached_sizes.rb
+++ tools/modules/update_payload_cached_sizes.rb
@@ -25,13 +25,19 @@ require 'rex'
 
 # Initialize the simplified framework instance.
 exceptions = []
-framework = Msf::Simple::Framework.create({'DeferModuleLoads' => true})
+
+# Create dummy config file to avoid using locally saved options
+dummy_config = File.expand_path(File.join(File.dirname(msfbase), '..', '..', 'spec', 'dummy','framework','config'))
+
+framework = Msf::Simple::Framework.create({'DeferModuleLoads' => true, 'ConfigDirectory' => dummy_config})
+module_set = framework.modules.module_set('payload')
+module_set.recalculate
 
 framework.payloads.each_module do |name, mod|
   begin
     next if name =~ /generic/
     next if name =~ /custom/
-    mod_inst = framework.payloads.create(name)
+    mod_inst = module_set.create(name)
     #mod_inst.datastore.merge!(framework.datastore)
     next if mod_inst.is_a?(Msf::Payload::Adapter) || Msf::Util::PayloadCachedSize.is_cached_size_accurate?(mod_inst)
     $stdout.puts "[*] Updating the CacheSize for #{mod.file_path} in #{name}..."
