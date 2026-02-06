4ec3bff82f0c22199ef06772688cb89a4d40d5e5
Diego Ledda <diego_ledda@rapid7.com>
2026-02-06 11:53:33 +0100

    fix: x86 windows with boilerplate utils/exe breakup

--- lib/msf/util/exe/windows/x86.rb
+++ lib/msf/util/exe/windows/x86.rb
@@ -1,27 +1,34 @@
+
 module Msf::Util::EXE::Windows::X86
-  
   include Msf::Util::EXE::Windows::Common
-  
+
   def self.included(base)
     base.extend(ClassMethods)
   end
 
   module ClassMethods
-    def to_win32pe_vbs(framework, code, opts = {})
-      Msf::Util::EXE::Windows::Common.to_exe_vbs(to_win32pe(framework, code, opts), opts)
+    # Construct a Windows x86 PE executable with the given shellcode.
+    # to_win32pe
+    #
+    # @param framework [Msf::Framework] The Metasploit framework instance.
+    # @param code [String] The shellcode to embed in the executable.
+    # @param opts [Hash] Additional options.
+    # @return [String] The constructed PE executable as a binary string.
+
+    def to_win32pe(framework, code, opts = {})
+      # Use the standard template if not specified by the user.
+      # This helper finds the full path and stores it in opts[:template].
+      set_template_default(opts, 'template_x86_windows.exe')
+
+      # Read the template directly from the path now stored in the options.
+      pe = File.read(opts[:template], mode: 'rb')
+
+      # Find the tag and inject the payload
+      bo = find_payload_tag(pe, 'Invalid Windows x86 template: missing "PAYLOAD:" tag')
+      pe[bo, code.length] = code.dup
+      pe
     end
     
-    def to_win32pe_dll(framework, code, opts = {})
-        flavor = opts.fetch(:mixed_mode, false) ? 'mixed_mode' : nil
-        Msf::Util::EXE::Windows::Common.set_template_default_winpe_dll(opts, ARCH_X86, code.size, flavor: flavor)
-        opts[:exe_type] = :dll
-
-        if opts[:inject]
-          to_win32pe(framework, code, opts)
-        else
-          Msf::Util::EXE::Windows::Common.exe_sub_method(code, opts)
-        end
-      end
     # to_win32pe
     #
     # @param  framework [Msf::Framework]
@@ -33,43 +40,44 @@ module Msf::Util::EXE::Windows::X86
     # @option opts      [Symbol] :arch, Set to :x86 by default
     # @return           [String]
     def to_win32pe(framework, code, opts = {})
+
       # For backward compatibility, this is roughly equivalent to 'exe-small' fmt
       if opts[:sub_method]
         if opts[:inject]
-          raise 'NOTE: using the substitution method means no inject support'
+          raise RuntimeError, 'NOTE: using the substitution method means no inject support'
         end
 
         # use
-        return to_win32pe_exe_sub(framework, code, opts)
+        return self.to_win32pe_exe_sub(framework, code, opts)
       end
 
       # Allow the user to specify their own EXE template
-      Msf::Util::EXE::Common.set_template_default(opts, 'template_x86_windows.exe')
+      set_template_default(opts, "template_x86_windows.exe")
 
       # Copy the code to a new RWX segment to allow for self-modifying encoders
-      payload = Msf::Util::EXE::Windows::Common.win32_rwx_exec(code)
+      payload = win32_rwx_exec(code)
 
       # Create a new PE object and run through sanity checks
       pe = Rex::PeParsey::Pe.new_from_file(opts[:template], true)
 
-      # try to inject code into executable by adding a section without affecting executable behavior
+      #try to inject code into executable by adding a section without affecting executable behavior
       if opts[:inject]
         injector = Msf::Exe::SegmentInjector.new({
-          payload: code,
-          template: opts[:template],
-          arch: :x86,
-          secname: opts[:secname]
+            :payload  => code,
+            :template => opts[:template],
+            :arch     => :x86,
+            :secname  => opts[:secname]
         })
         return injector.generate_pe
       end
 
       text = nil
-      pe.sections.each { |sec| text = sec if sec.name == '.text' }
+      pe.sections.each {|sec| text = sec if sec.name == ".text"}
 
-      raise 'No .text section found in the template' unless text
+      raise RuntimeError, "No .text section found in the template" unless text
 
       unless text.contains_rva?(pe.hdr.opt.AddressOfEntryPoint)
-        raise 'The .text section does not contain an entry point'
+        raise RuntimeError, "The .text section does not contain an entry point"
       end
 
       p_length = payload.length + 256
@@ -77,16 +85,16 @@ module Msf::Util::EXE::Windows::X86
       # If the .text section is too small, append a new section instead
       if text.size < p_length
         appender = Msf::Exe::SegmentAppender.new({
-          payload: code,
-          template: opts[:template],
-          arch: :x86,
-          secname: opts[:secname]
+            :payload  => code,
+            :template => opts[:template],
+            :arch     => :x86,
+            :secname  => opts[:secname]
         })
         return appender.generate_pe
       end
 
       # Store some useful offsets
-      pe.rva_to_file_offset(pe.hdr.opt.AddressOfEntryPoint)
+      off_ent = pe.rva_to_file_offset(pe.hdr.opt.AddressOfEntryPoint)
       off_beg = pe.rva_to_file_offset(text.base_rva)
 
       # We need to make sure our injected code doesn't conflict with the
@@ -95,18 +103,17 @@ module Msf::Util::EXE::Windows::X86
       pe.hdr.opt['DataDirectory'].each do |dir|
         next if dir.v['Size'] == 0
         next unless text.contains_rva?(dir.v['VirtualAddress'])
-
         delta = pe.rva_to_file_offset(dir.v['VirtualAddress']) - off_beg
         mines << [delta, dir.v['Size']]
       end
 
       # Break the text segment into contiguous blocks
       blocks = []
-      bidx = 0
-      mines.sort { |a, b| a[0] <=> b[0] }.each do |mine|
+      bidx   = 0
+      mines.sort{|a,b| a[0] <=> b[0]}.each do |mine|
         bbeg = bidx
         bend = mine[0]
-        blocks << [bidx, bend - bidx] if bbeg != bend
+        blocks << [bidx, bend-bidx] if bbeg != bend
         bidx = mine[0] + mine[1]
       end
 
@@ -114,16 +121,16 @@ module Msf::Util::EXE::Windows::X86
       blocks << [bidx, text.size - bidx] if bidx < text.size - 1
 
       # Find the largest contiguous block
-      blocks.sort! { |a, b| b[1] <=> a[1] }
+      blocks.sort!{|a,b| b[1]<=>a[1]}
       block = blocks.first
 
       # TODO: Allow the entry point in a different block
       if payload.length + 256 >= block[1]
-        raise "The largest block in .text does not have enough contiguous space (need:#{payload.length + 257} found:#{block[1]})"
+        raise RuntimeError, "The largest block in .text does not have enough contiguous space (need:#{payload.length+257} found:#{block[1]})"
       end
 
       # Make a copy of the entire .text section
-      data = text.read(0, text.size)
+      data = text.read(0,text.size)
 
       # Pick a random offset to store the payload
       poff = rand(block[1] - payload.length - 256)
@@ -133,13 +140,13 @@ module Msf::Util::EXE::Windows::X86
       eidx = nil
 
       # Pad the entry point with random nops
-      entry = generate_nops(framework, [ARCH_X86], rand(51..250))
+      entry = generate_nops(framework, [ARCH_X86], rand(200) + 51)
 
       # Pick an offset to store the new entry point
       if eloc == 0 # place the entry point before the payload
         poff += 256
-        eidx = rand(poff - (entry.length + 5))
-      else # place the entry pointer after the payload
+        eidx = rand(poff-(entry.length + 5))
+      else          # place the entry pointer after the payload
         poff -= [256, poff].min
         eidx = rand(block[1] - (poff + payload.length + 256)) + poff + payload.length
       end
@@ -149,27 +156,27 @@ module Msf::Util::EXE::Windows::X86
 
       # Mangle 25% of the original executable
       1.upto(block[1] / 4) do
-        data[block[0] + rand(block[1]), 1] = [rand(0x100)].pack('C')
+        data[ block[0] + rand(block[1]), 1] = [rand(0x100)].pack("C")
       end
 
       # Patch the payload and the new entry point into the .text
       data[block[0] + poff, payload.length] = payload
-      data[block[0] + eidx, entry.length] = entry
+      data[block[0] + eidx, entry.length]   = entry
 
       # Create the modified version of the input executable
       exe = ''
-      File.open(opts[:template], 'rb') { |fd| exe = fd.read(fd.stat.size) }
+      File.open(opts[:template], 'rb') {|fd| exe = fd.read(fd.stat.size)}
 
-      a = [text.base_rva + block.first + eidx].pack('V')
+      a = [text.base_rva + block.first + eidx].pack("V")
       exe[exe.index([pe.hdr.opt.AddressOfEntryPoint].pack('V')), 4] = a
       exe[off_beg, data.length] = data
 
       tds = pe.hdr.file.TimeDateStamp
-      exe[exe.index([tds].pack('V')), 4] = [tds - rand(0x1000000)].pack('V')
+      exe[exe.index([tds].pack('V')), 4] = [tds - rand(0x1000000)].pack("V")
 
       cks = pe.hdr.opt.CheckSum
       unless cks == 0
-        exe[exe.index([cks].pack('V')), 4] = [0].pack('V')
+        exe[exe.index([cks].pack('V')), 4] = [0].pack("V")
       end
 
       exe = clear_dynamic_base(exe, pe)
@@ -184,14 +191,15 @@ module Msf::Util::EXE::Windows::X86
     # @param code       [String]
     # @param opts       [Hash]
     # @param arch       [String] Default is "x86"
-    def to_winpe_only(framework, code, opts = {}, arch = ARCH_X86)
+    def to_winpe_only(framework, code, opts = {}, arch=ARCH_X86)
+
       # Allow the user to specify their own EXE template
-      Msf::Util::EXE::Common.set_template_default(opts, "template_#{arch}_windows.exe")
+      set_template_default(opts, "template_#{arch}_windows.exe")
 
       pe = Rex::PeParsey::Pe.new_from_file(opts[:template], true)
 
       exe = ''
-      File.open(opts[:template], 'rb') { |fd| exe = fd.read(fd.stat.size) }
+      File.open(opts[:template], 'rb') {|fd| exe = fd.read(fd.stat.size)}
 
       pe_header_size = 0x18
       entryPoint_offset = 0x28
@@ -212,7 +220,7 @@ module Msf::Util::EXE::Windows::X86
         section_offset = sections_table_offset + (i * section_size)
         sections_header << [
           sections_table_characteristics_offset + (i * section_size),
-          exe[section_offset, section_size]
+          exe[section_offset,section_size]
         ]
       end
 
@@ -220,29 +228,29 @@ module Msf::Util::EXE::Windows::X86
 
       # look for section with entry point
       sections_header.each do |sec|
-        virtualAddress = sec[1][virtualAddress_offset, 0x4].unpack('V')[0]
-        sizeOfRawData = sec[1][sizeOfRawData_offset, 0x4].unpack('V')[0]
-        characteristics = sec[1][characteristics_offset, 0x4].unpack('V')[0]
+        virtualAddress = sec[1][virtualAddress_offset,0x4].unpack('V')[0]
+        sizeOfRawData = sec[1][sizeOfRawData_offset,0x4].unpack('V')[0]
+        characteristics = sec[1][characteristics_offset,0x4].unpack('V')[0]
 
-        next unless (virtualAddress...virtualAddress + sizeOfRawData).include?(addressOfEntryPoint)
+        if (virtualAddress...virtualAddress+sizeOfRawData).include?(addressOfEntryPoint)
+          importsTable = pe.hdr.opt.DataDirectory[8..(8+4)].unpack('V')[0]
+          if (importsTable - addressOfEntryPoint) < code.length
+            #shift original entry point to prevent tables overwriting
+            addressOfEntryPoint = importsTable - code.length + 4
 
-        importsTable = pe.hdr.opt.DataDirectory[8..(8 + 4)].unpack('V')[0]
-        if (importsTable - addressOfEntryPoint) < code.length
-          # shift original entry point to prevent tables overwriting
-          addressOfEntryPoint = importsTable - code.length + 4
-
-          entry_point_offset = pe._dos_header.v['e_lfanew'] + entryPoint_offset
-          exe[entry_point_offset, 4] = [addressOfEntryPoint].pack('V')
+            entry_point_offset = pe._dos_header.v['e_lfanew'] + entryPoint_offset
+            exe[entry_point_offset,4] = [addressOfEntryPoint].pack('V')
+          end
+          # put this section writable
+          characteristics |= 0x8000_0000
+          newcharacteristics = [characteristics].pack('V')
+          exe[sec[0],newcharacteristics.length] = newcharacteristics
         end
-        # put this section writable
-        characteristics |= 0x8000_0000
-        newcharacteristics = [characteristics].pack('V')
-        exe[sec[0], newcharacteristics.length] = newcharacteristics
       end
 
       # put the shellcode at the entry point, overwriting template
       entryPoint_file_offset = pe.rva_to_file_offset(addressOfEntryPoint)
-      exe[entryPoint_file_offset, code.length] = code
+      exe[entryPoint_file_offset,code.length] = code
       exe = clear_dynamic_base(exe, pe)
       exe
     end
@@ -253,55 +261,52 @@ module Msf::Util::EXE::Windows::X86
     # @param  code      [String]
     # @param  opts      [Hash]
     def to_win32pe_old(framework, code, opts = {})
+
       payload = code.dup
       # Allow the user to specify their own EXE template
-      Msf::Util::EXE::Common.set_template_default(opts, 'template_x86_windows_old.exe')
+      set_template_default(opts, "template_x86_windows_old.exe")
 
       pe = ''
-      File.open(opts[:template], 'rb') { |fd| pe = fd.read(fd.stat.size) }
+      File.open(opts[:template], "rb") {|fd| pe = fd.read(fd.stat.size)}
 
       if payload.length <= 2048
-        payload << Rex::Text.rand_text(2048 - payload.length)
+        payload << Rex::Text.rand_text(2048-payload.length)
       else
-        raise 'The EXE generator now has a max size of 2048 ' +
-              'bytes, please fix the calling module'.to_s
+        raise RuntimeError, "The EXE generator now has a max size of 2048 " +
+                            "bytes, please fix the calling module"
       end
 
       bo = pe.index('PAYLOAD:')
       unless bo
-        raise 'Invalid Win32 PE OLD EXE template: missing "PAYLOAD:" tag'
+        raise RuntimeError, "Invalid Win32 PE OLD EXE template: missing \"PAYLOAD:\" tag"
       end
-
       pe[bo, payload.length] = payload
 
       pe[136, 4] = [rand(0x100000000)].pack('V')
 
       ci = pe.index("\x31\xc9" * 160)
       unless ci
-        raise 'Invalid Win32 PE OLD EXE template: missing first "\\x31\\xc9"'
+        raise RuntimeError, "Invalid Win32 PE OLD EXE template: missing first \"\\x31\\xc9\""
       end
-
       cd = pe.index("\x31\xc9" * 160, ci + 320)
       unless cd
-        raise 'Invalid Win32 PE OLD EXE template: missing second "\\x31\\xc9"'
+        raise RuntimeError, "Invalid Win32 PE OLD EXE template: missing second \"\\x31\\xc9\""
       end
-
-      rc = pe[ci + 320, cd - ci - 320]
+      rc = pe[ci+320, cd-ci-320]
 
       # 640 + rc.length bytes of room to store an encoded rc at offset ci
       enc = encode_stub(framework, [ARCH_X86], rc, ::Msf::Module::PlatformList.win32)
-      rc.length
-      enc.length
+      lft = 640+rc.length - enc.length
 
-      buf = enc + Rex::Text.rand_text(640 + rc.length - enc.length)
+      buf = enc + Rex::Text.rand_text(640+rc.length - enc.length)
       pe[ci, buf.length] = buf
 
       # Make the data section executable
       xi = pe.index([0xc0300040].pack('V'))
-      pe[xi, 4] = [0xe0300020].pack('V')
+      pe[xi,4] = [0xe0300020].pack('V')
 
       # Add a couple random bytes for fun
-      pe << Rex::Text.rand_text(rand(4..67))
+      pe << Rex::Text.rand_text(rand(64)+4)
       pe
     end
 
@@ -313,9 +318,9 @@ module Msf::Util::EXE::Windows::X86
     # @return           [String]
     def to_win32pe_exe_sub(framework, code, opts = {})
       # Allow the user to specify their own DLL template
-      Msf::Util::EXE::Common.set_template_default(opts, 'template_x86_windows.exe')
+      set_template_default(opts, "template_x86_windows.exe")
       opts[:exe_type] = :exe_sub
-      exe_sub_method(code, opts)
+      exe_sub_method(code,opts)
     end
 
     # Embeds shellcode within a Windows PE file implementing the Windows
@@ -331,10 +336,12 @@ module Msf::Util::EXE::Windows::X86
     # @return [String] Windows Service PE file
     def to_win32pe_service(framework, code, opts = {})
       # Allow the user to specify their own service EXE template
-      Msf::Util::EXE::Common.set_template_default(opts, 'template_x86_windows_svc.exe')
+      set_template_default(opts, "template_x86_windows_svc.exe")
       opts[:exe_type] = :service_exe
-      exe_sub_method(code, opts)
+      exe_sub_method(code,opts)
     end
+
+
 
     # to_win32pe_dccw_gdiplus_dll
     #
@@ -346,11 +353,10 @@ module Msf::Util::EXE::Windows::X86
     # @option           [String] :inject
     # @return           [String]
     def to_win32pe_dccw_gdiplus_dll(framework, code, opts = {})
-      Msf::Util::EXE::Windows::Common.set_template_default_winpe_dll(opts, ARCH_X86, code.size, flavor: 'dccw_gdiplus')
+      set_template_default_winpe_dll(opts, ARCH_X86, code.size, flavor: 'dccw_gdiplus')
       to_win32pe_dll(framework, code, opts)
     end
   end
-
   class << self
     include ClassMethods
   end
