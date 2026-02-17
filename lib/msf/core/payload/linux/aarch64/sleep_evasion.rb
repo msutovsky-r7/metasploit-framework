438e3a23b44c5f6c3b192307fd978fb82e656d12
litemars <44295342+litemars@users.noreply.github.com>
2026-02-17 22:09:36 +0100

    Update lib/msf/core/payload/linux/aarch64/sleep_evasion.rb
    Co-authored-by: Diego Ledda <diego_ledda@rapid7.com>

--- lib/msf/core/payload/linux/aarch64/sleep_evasion.rb
+++ lib/msf/core/payload/linux/aarch64/sleep_evasion.rb
@@ -3,24 +3,16 @@ module Msf::Payload::Linux::Aarch64::SleepEvasion
     STUB_SLEEP_SECONDS_OFFSET = 0x04
 
     def sleep_stub
-      stub = ""
-
-      # 0x00: b 0x14                     ; branch forward to code (skip data section)
-      stub << [0x14000005].pack('V')
-      # 0x04: timespec.tv_sec (8 bytes)  ; sleep duration in seconds (patched later)
-      stub << "\x00\x00\x00\x00\x00\x00\x00\x00"
-      # 0x0c: timespec.tv_nsec (8 bytes) ; nanoseconds component (always 0)
-      stub << "\x00\x00\x00\x00\x00\x00\x00\x00"
-      # 0x14: adr x0, 0x04               ; x0 -> timespec structure
-      stub << [0x10ffff80].pack('V')
-      # 0x18: mov x1, #0                 ; x1 = NULL (remaining time pointer)
-      stub << [0xd2800001].pack('V')
-      # 0x1c: mov x8, #101               ; syscall number for nanosleep
-      stub << [0xd2800ca8].pack('V')
-      # 0x20: svc #0                     ; invoke syscall
-      stub << [0xd4000001].pack('V')
-      # 0x24: execution continues to appended payload
-
+      stub = [ 0x14000005,     # 0x00: b 0x14    ; branch forward to code (skip data section)
+               0x00000000,     # 0x04: timespec.tv_sec (8 bytes)  ; sleep duration in seconds (patched later)
+               0x00000000,
+               0x00000000,     # 0x0c: timespec.tv_nsec (8 bytes) ; nanoseconds component (always 0)
+               0x00000000,
+               0x10ffff80,     # 0x14: adr x0, 0x04               ; x0 -> timespec structure
+               0xd2800001,     # 0x18: mov x1, #0                 ; x1 = NULL (remaining time pointer)
+               0xd2800ca8,     # 0x1c: mov x8, #101               ; syscall number for nanosleep
+               0xd4000001      # 0x20: svc #0                     ; invoke syscall
+               ].pack('V*')    # 0x24: execution continues to appended payload
       stub
     end
 
